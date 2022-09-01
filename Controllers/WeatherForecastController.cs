bf6963cf433e02e0700902fb6a3c515d3c8feedf
ruchva <ruchva@gmail.com>
2022-08-31 21:57:22 -0400

    get, post, delete

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,65 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    /// <summary>
+    /// nuevo objeto weather
+    /// </summary>
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        
+        /// add new return
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
     }
 
+    /// <summary>
+    /// add objeto lista
+    /// </summary>
+    /// <returns></returns>
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //{
+        //    Date = DateTime.Now.AddDays(index),
+        //    TemperatureC = Random.Shared.Next(-20, 55),
+        //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //})
+        //.ToArray();
+        return ListWeatherForecast;
+    }
+
+    /// <summary>
+    /// add post method
+    /// </summary>
+    /// <param name="weatherForecast"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForecast.Add(weatherForecast);
+        return Ok();
+    }
+
+    /// <summary>
+    /// add delete method
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    [HttpDelete]
+    public IActionResult Delete(int index)
+    {
+        ListWeatherForecast.RemoveAt(index);
+        return Ok();
     }
 }
